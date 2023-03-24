ec1eff4f6ef87888f99cffec55435bdb1f776e01
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-03-24 23:39:14 +0530

    Parttime Wage Check

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
-﻿namespace EmployeeWageComputationProblem
+﻿using NonStaticEmpWageProblem;
+
+namespace EmployeeWageComutationProblem
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to employee wage computation problem");
-
-            EmployeeDailyWage.EmployeeWage();
+            EmpPartTimeWage Wage = new EmpPartTimeWage();
+            Wage.PartTimeEmployee();
         }
     }
 }
