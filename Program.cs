845c7df7c1eb663e450402dc49771e5a4249724c
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-03-24 23:22:03 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWageComputationProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation problem");
+        }
+    }
+}
