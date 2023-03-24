336a90e529d2ac79fbda943928a527f1e357cfd7
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-03-24 23:33:12 +0530

    Wage Check

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to employee wage computation problem");
-            EmployeeAttendence.EmployeeCheck();
+
+            EmployeeDailyWage.EmployeeWage();
         }
     }
 }
